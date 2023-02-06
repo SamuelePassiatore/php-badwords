2d6823e8e6dadb0bf20e7f33a4a1665549fe017d
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-06 16:20:12 +0100

    first commit

---
+++ badwords.php
@@ -0,0 +1,21 @@
+<?php
+$censored_word = $_GET['censure'];
+$paragraph = $_GET['paragraph'];
+?>
+ 
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+    <title>Document</title>
+</head>
+<body>
+    <h2>Paragrafo Originale</h2>
+    <p><?= $paragraph; ?></p>
+    <p>ecco</p>
+</body>
+</html>
